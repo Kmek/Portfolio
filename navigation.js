c0dba6f51d41b82381b77be7062d84c7b517638d
Kmek <kmekeefe@gmail.com>
2019-11-28 10:42:21 -0500

    Home navigates to index-buttons.html

--- navigation.js
+++ navigation.js
@@ -4,7 +4,7 @@
 const navBtn = document.getElementById("navBtn")
 
 const navTitles = ["Home", "Diffusion Sim", "Pathfinding Sim", "Vector Solver", "Web Design", "CS Courses", "Echo Internship", "5106C Robotics", "GrabCAD", "Awards", "Volunteering", "View On GitHub"]
-const navLinks = ["index.html", "diffusion.html", "pathfinding.html", "vectorsolver.html", "websites.html", "cs50.html", "internship.html", "robotics.html", "grabcad.html", "awards.html", "hours.html", "https://github.com/Kmek/Portfolio"]
+const navLinks = ["index-buttons.html", "diffusion.html", "pathfinding.html", "vectorsolver.html", "websites.html", "cs50.html", "internship.html", "robotics.html", "grabcad.html", "awards.html", "hours.html", "https://github.com/Kmek/Portfolio"]
 
 let navDots = document.createElement("h2")
 navDots.innerHTML = "..."
